bc024eede1912a3529c8e791ea1308413e03b755
Sumit kumar <sumitkumar@Sumits-MacBook-Air.local>
2020-06-30 01:18:47 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name="loremipsumpy",
+    version="0.0.1",
+    description="This module generates lorem ipsum placeholder Text",
+    py_modules = ["lorem"],
+    package_dir={'':'src'},
+    author="Sumit",
+    author_email="sb78639@gmai.com",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Opearating Sytem :: OS Independent"
+    ]
+)
